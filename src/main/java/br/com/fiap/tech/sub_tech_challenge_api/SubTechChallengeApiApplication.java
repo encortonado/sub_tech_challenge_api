e7a13097fb91d94a4fd66fd91f481c26bee0bc76
Lucas Bortolo <lucasbortolo3@gmail.com>
2024-09-06 09:29:20 -0300

    First Commit Scaffolding

---
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_api/SubTechChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTechChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubTechChallengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/tech/sub_tech_challenge_api/SubTechChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubTechChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
