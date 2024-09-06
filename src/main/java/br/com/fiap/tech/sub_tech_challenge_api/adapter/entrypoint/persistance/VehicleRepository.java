be3f160cce4284b3d6ecef975795bc947733fdbc
Lucas Bortolo <lucasbortolo3@gmail.com>
2024-09-06 12:41:26 -0300

    testes para listar veiculos com order pelo preco

--- src/main/java/br/com/fiap/tech/sub_tech_challenge_api/adapter/entrypoint/persistance/VehicleRepository.java
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_api/adapter/entrypoint/persistance/VehicleRepository.java
@@ -3,5 +3,10 @@ package br.com.fiap.tech.sub_tech_challenge_api.adapter.entrypoint.persistance;
 import br.com.fiap.tech.sub_tech_challenge_api.application.vehicle.entities.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VehicleRepository extends JpaRepository<VehicleEntity, Long> {
+
+    List<VehicleEntity> findAllByOrderByPriceAsc();
+
 }
--- src/test/java/br/com/fiap/tech/sub_tech_challenge_api/adapter/entrypoint/persistance/VehicleRepositoryTest.java
+++ src/test/java/br/com/fiap/tech/sub_tech_challenge_api/adapter/entrypoint/persistance/VehicleRepositoryTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -74,6 +76,37 @@ class VehicleRepositoryTest {
                         .isEqualTo(vehicleEntity);
 
                 verify(vehicleRepository, times(1)).save(any(VehicleEntity.class));
+            }
+
+        }
+
+        @Nested
+        class listAllVehicles {
+
+            @Nested
+            class listVehicles {
+
+                @Test
+                void listAllVehiclesTest() {
+                    VehicleEntity vehicleEntity = TestUtils.buildVehicleEntity();
+                    VehicleEntity vehicleEntity2 = TestUtils.anotherBuildVehicleEntity();
+
+                    List<VehicleEntity> vehicleEntityList = new ArrayList<>();
+                    vehicleEntityList.add(vehicleEntity);
+                    vehicleEntityList.add(vehicleEntity2);
+
+                    when(vehicleRepository.findAllByOrderByPriceAsc()).thenReturn(vehicleEntityList);
+
+                    var vehicleList = vehicleRepository.findAllByOrderByPriceAsc();
+                    Assertions.assertThat(vehicleList)
+                            .isNotNull()
+                            .isInstanceOf(List.class)
+                            .isEqualTo(vehicleEntityList);
+
+                    verify(vehicleRepository, times(1)).findAllByOrderByPriceAsc();
+
+                }
+
             }
 
         }
