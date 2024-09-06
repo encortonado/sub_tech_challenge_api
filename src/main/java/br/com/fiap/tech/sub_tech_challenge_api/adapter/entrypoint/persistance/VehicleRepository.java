f69ff426284a77f7f9fc430578f78a79bc8d7d3d
Lucas Bortolo <lucasbortolo3@gmail.com>
2024-09-06 20:28:30 -0300

    adicionando annotations

--- src/main/java/br/com/fiap/tech/sub_tech_challenge_api/adapter/entrypoint/persistance/VehicleRepository.java
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_api/adapter/entrypoint/persistance/VehicleRepository.java
@@ -2,9 +2,11 @@ package br.com.fiap.tech.sub_tech_challenge_api.adapter.entrypoint.persistance;
 
 import br.com.fiap.tech.sub_tech_challenge_api.application.vehicle.entities.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<VehicleEntity, Long> {
 
     List<VehicleEntity> findAllByOrderByPriceAsc();
