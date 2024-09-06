32219b08f36ee480f06ea3b5b2baf721e86689f5
Lucas Bortolo <lucasbortolo3@gmail.com>
2024-09-06 13:29:02 -0300

    Use cases de veiculo

---
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_api/application/vehicle/ports/IVehicleService.java
@@ -0,0 +1,7 @@
+package br.com.fiap.tech.sub_tech_challenge_api.application.vehicle.ports;
+
+public interface IVehicleService extends
+    IRegisterVehicleUseCase,
+    IUpdateVehicleRegisterUseCase,
+    IListVehiclesUseCase {
+}
