a5877225ae086ce76b4dedff49584ae6d5ab5724
Lucas Bortolo <lucasbortolo3@gmail.com>
2024-09-07 14:20:19 -0300

    config de seguranca barrando a compra de carro com bearer elaborada

--- src/main/java/br/com/fiap/tech/sub_tech_challenge_api/adapter/entrypoint/api/controller/VehicleController.java
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_api/adapter/entrypoint/api/controller/VehicleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController()
-@RequestMapping("/vehicle")
+@RequestMapping("/api/vehicle")
 public class VehicleController {
 
 
